1c60026a1992c3da3767d7bf9096f550c32c5816
Jurij <jurkastl@gmail.com>
2026-08-30 20:45:04 +0200

    Adopt the jurkast application ID

--- restlos/__init__.py
+++ restlos/__init__.py
@@ -2,9 +2,9 @@
 
 from __future__ import annotations
 
-__version__ = "1.4.2"
-APP_ID = "io.github.jurkastl.Restlos"
-LEGACY_APP_IDS = ("io.github.local.Restlos",)
+__version__ = "1.4.3"
+APP_ID = "io.github.jurkast.Restlos"
+LEGACY_APP_IDS = ("io.github.jurkastl.Restlos", "io.github.local.Restlos")
 APP_NAME = "Restlos Uninstaller"
 APP_TAGLINE = "Safe Linux App & Game Uninstaller"
-PROJECT_URL = "https://github.com/jurkastl/restlos"
+PROJECT_URL = "https://github.com/jurkast/restlos"
